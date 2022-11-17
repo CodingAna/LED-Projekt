2b578c286347a703469e1638b009b58e2dcde023
Ana <lukas@baginski.mobi>
2022-11-17 22:55:14 +0100

    Added project data

---
+++ src/EineWeitereKlasse.java
@@ -0,0 +1,11 @@
+import ledControl.BoardController;
+
+public class EineWeitereKlasse {
+	//Von UmbennnenMain übergebener Boardcontroller
+	private BoardController controller;
+	
+	//Konstruktor
+		public EineWeitereKlasse(BoardController controller) {
+			this.controller = controller;
+		}
+}
---
+++ src/PhasmaMain.java
@@ -0,0 +1,17 @@
+import java.awt.event.KeyEvent;
+import ledControl.BoardController;
+import ledControl.gui.KeyBuffer;
+
+public class PhasmaMain {
+
+	public static void main(String[] args) {
+		//größeres Board falls gewünscht
+//		BoardController.getBoardController(LedConfiguration.LED_20x20_EMULATOR);
+		BoardController controller = BoardController.getBoardController();
+		
+		//Das Objekt in der anderen Klasse wird erstellt
+		EineWeitereKlasse ewk = new EineWeitereKlasse(controller);
+
+	}
+
+}
