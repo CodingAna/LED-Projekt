f8c35673e26bae0f17d9f9fc8b1807fa73797231
Ana <lukas@baginski.mobi>
2022-11-23 19:50:32 +0100

    renamed Mine to Field + added attributes

---
+++ src/Field.java
@@ -0,0 +1,14 @@
+
+public class Field {
+	
+	private int x;
+	private int y;
+	private boolean isBomb;
+	
+	public Field(int x, int y, boolean isBomb) {
+		this.x = x;
+		this.y = y;
+		this.isBomb = isBomb;
+	}
+	
+}
