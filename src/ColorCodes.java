13b68b29a798b60a11711483853997ad80f1f295
Ana <lukas@baginski.mobi>
2022-12-04 03:24:16 +0100

    Enums with Color values

--- src/ColorCodes.java
+++ src/ColorCodes.java
@@ -1,14 +1,24 @@
 
 public enum ColorCodes {
-	BLACK,
-	WHITE,
-	YELLOW, //Flag
-	BLUE, // One
-	GREEN, // Two
-	RED, // Three
-	PURPLE, // Four
-	BROWN, // Five
-	TEAL, // Six
-	PINK, // Seven
-	GREY, // Eight
+	BLACK(new Color(0, 0, 0)),
+	WHITE(new Color(235, 235, 235)),
+	YELLOW(new Color(235, 235, 31)), //Flag
+	BLUE(new Color(1, 0, 234)), // One
+	GREEN(new Color(1, 235, 1)), // Two
+	RED(new Color(234, 0, 0)), // Three
+	PURPLE(new Color(200, 0, 200)), // Four
+	BROWN(new Color(186, 98, 31)), // Five
+	TEAL(new Color(0, 128, 129)), // Six
+	PINK(new Color(235, 85, 160)), // Seven
+	GREY(new Color(128, 128, 128)); // Eight
+	
+	private Color color;
+	
+	private ColorCodes(Color color) {
+		this.color = color;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
 }
