5c456399b3f7d272d4c028875313be2faef29b1f
Ana <lukas@baginski.mobi>
2023-01-03 17:13:58 +0100

    Renamed ewk variables

--- src/Player.java
+++ src/Player.java
@@ -10,7 +10,7 @@ public class Player {
 	private KeyBuffer buffer;
 	private KeyEvent event;
 	private BoardKeyListener listener;
-	private GameHandler ewk;
+	private GameHandler handler;
 
 	private boolean moved;
 	private boolean died;
@@ -20,9 +20,9 @@ public class Player {
 	private int oldX = 0;
 	private int oldY = 0;
 	
-	public Player(BoardController controller, GameHandler ewk) 
+	public Player(BoardController controller, GameHandler handler) 
 	{
-		this.ewk = ewk;
+		this.handler = handler;
 		this.controller = controller;
 		this.buffer = KeyBuffer.getKeyBuffer();
 		this.listener = new BoardKeyListener(buffer);
@@ -42,27 +42,27 @@ public class Player {
 				switch(event.getKeyCode())
 				{
 				case 10: // revealing current field
-					if (ewk.mineField[x][y].isBomb()) {
+					if (handler.mineField[x][y].isBomb()) {
 						this.died = true;
 					}
-					if(!ewk.mineField[x][y].isRevealed() && !ewk.mineField[x][y].isFlagged())
+					if(!handler.mineField[x][y].isRevealed() && !handler.mineField[x][y].isFlagged())
 					{
-						ewk.coloring(x, y, 0);
+						handler.coloring(x, y, 0);
 					}
 					break;
 				case 32: //setting flag on current field
-					if(!ewk.mineField[x][y].isFlagged() && !ewk.mineField[x][y].isRevealed())
+					if(!handler.mineField[x][y].isFlagged() && !handler.mineField[x][y].isRevealed())
 					{
-						ewk.mineField[x][y].setFlag(true);
-						ewk.controllerSetColor(x, y, ColorCodes.YELLOW.getColor());
+						handler.mineField[x][y].setFlag(true);
+						handler.controllerSetColor(x, y, ColorCodes.YELLOW.getColor());
 						controller.updateBoard();
 					}
 					else
 					{
-						if (ewk.mineField[x][y].isFlagged() && !ewk.mineField[x][y].isRevealed())
+						if (handler.mineField[x][y].isFlagged() && !handler.mineField[x][y].isRevealed())
 						{
-							ewk.mineField[x][y].setFlag(false);
-							ewk.controllerSetColor(x, y, ColorCodes.GREY.getColor());
+							handler.mineField[x][y].setFlag(false);
+							handler.controllerSetColor(x, y, ColorCodes.GREY.getColor());
 							controller.updateBoard();
 						}
 					}
@@ -101,23 +101,23 @@ public class Player {
 				
 				if (moved)
 				{
-					ewk.triggerMovement(x, y, oldX, oldY);
+					handler.triggerMovement(x, y, oldX, oldY);
 					moved = false;
 					oldX = x;
 					oldY = y;
-					System.out.println("Flagge: " + ewk.mineField[x][y].isFlagged() + ", Revealed: " + ewk.mineField[x][y].isRevealed() + ", Bombs: " + ewk.mineField[x][y].getAdj() + ", isBomb: " + ewk.mineField[x][y].isBomb());
+					System.out.println("Flagge: " + handler.mineField[x][y].isFlagged() + ", Revealed: " + handler.mineField[x][y].isRevealed() + ", Bombs: " + handler.mineField[x][y].getAdj() + ", isBomb: " + handler.mineField[x][y].isBomb());
 				}				
 			}
 		}
 		// The player died and while loop ended
 		long length;
-		length = ewk.playAudio("bomb_explosion");
+		length = handler.playAudio("bomb_explosion");
 		
 		// Make a RED / WHITE flashing animation
 		for (int switching=0; switching<6; switching++) {
 			for (int y=0; y<20; y++) {
 				for (int x=0; x<20; x++) {
-					ewk.controllerSetColor(x, y, ((switching % 2 == 0) ? ColorCodes.RED : ColorCodes.WHITE).getColor());
+					handler.controllerSetColor(x, y, ((switching % 2 == 0) ? ColorCodes.RED : ColorCodes.WHITE).getColor());
 				}				
 			}
 			controller.updateBoard();
@@ -126,18 +126,18 @@ public class Player {
 		
 		length = (int)(length / 1000) - (500 * 6);
 		if (length > 0) controller.sleep((int)length);
-		length = ewk.playAudio("sad_trombone");
+		length = handler.playAudio("sad_trombone");
 		
 		// Reset board to all BLACK
 		for (int y=0; y<20; y++) {
 			for (int x=0; x<20; x++) {
-				ewk.controllerSetColor(x, y, ColorCodes.BLACK.getColor());
+				handler.controllerSetColor(x, y, ColorCodes.BLACK.getColor());
 			}				
 		}
 		controller.updateBoard();
 		
 		controller.sleep((int)(length / 1000));
-		ewk.playAudio("sad_violin_airhorn");
+		handler.playAudio("sad_violin_airhorn");
 		
 		boolean flashing = true;
 		int count = 0;
@@ -145,37 +145,37 @@ public class Player {
 			// Left Eye
 			for (int y=0; y<2; y++)
 				for (int x=0; x<2; x++)
-					ewk.controllerSetColor(x+5, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+					handler.controllerSetColor(x+5, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 		
 			// Right Eye
 			for (int y=0; y<2; y++)
 				for (int x=0; x<2; x++)
-					ewk.controllerSetColor(x+12, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+					handler.controllerSetColor(x+12, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 			
 			// Mouth Left Lower
 			for (int y=0; y<4; y++)
 				for (int x=0; x<2; x++)
-					ewk.controllerSetColor(x+3, y+13, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+					handler.controllerSetColor(x+3, y+13, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 			
 			// Mouth Left Middle
 			for (int y=0; y<2; y++)
 				for (int x=0; x<2; x++)
-					ewk.controllerSetColor(x+4, y+12, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+					handler.controllerSetColor(x+4, y+12, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 			
 			// Mouth Middle
 			for (int y=0; y<2; y++)
 				for (int x=0; x<7; x++)
-					ewk.controllerSetColor(x+6, y+11, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+					handler.controllerSetColor(x+6, y+11, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 			
 			// Mouth Right Middle
 			for (int y=0; y<2; y++)
 				for (int x=0; x<2; x++)
-					ewk.controllerSetColor(x+13, y+12, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+					handler.controllerSetColor(x+13, y+12, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 			
 			// Mouth Right Lower
 			for (int y=0; y<4; y++)
 				for (int x=0; x<2; x++)
-					ewk.controllerSetColor(x+14, y+13, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+					handler.controllerSetColor(x+14, y+13, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 			
 			controller.updateBoard();
 			controller.sleep(840);
@@ -187,21 +187,21 @@ public class Player {
 		
 		flashing = true;
 		
-		ewk.playAudio("mlg_airhorn");
+		handler.playAudio("mlg_airhorn");
 		for (int i=0; i<2; i++) {
 			laughingSmiley(flashing);
 			flashing = !flashing;
 			controller.sleep(700);
 		}
 		
-		ewk.playAudio("mlg_triple");
+		handler.playAudio("mlg_triple");
 		for (int i=0; i<7; i++) {
 			laughingSmiley(flashing);
 			flashing = !flashing;
 			controller.sleep(400);
 		}
 		
-		ewk.playAudio("mlg_cancan");
+		handler.playAudio("mlg_cancan");
 		while (true) {
 			laughingSmiley(flashing);
 			flashing = !flashing;
@@ -213,42 +213,42 @@ public class Player {
 		// Whole background
 		for (int y=0; y<20; y++)
 			for (int x=0; x<20; x++)
-				ewk.controllerSetColor(x, y, (flashing ? ColorCodes.BLACK : ColorCodes.WHITE).getColor());
+				handler.controllerSetColor(x, y, (flashing ? ColorCodes.BLACK : ColorCodes.WHITE).getColor());
 		
 		// Left Eye
 		for (int y=0; y<2; y++)
 			for (int x=0; x<2; x++)
-				ewk.controllerSetColor(x+5, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+				handler.controllerSetColor(x+5, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 	
 		// Right Eye
 		for (int y=0; y<2; y++)
 			for (int x=0; x<2; x++)
-				ewk.controllerSetColor(x+12, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+				handler.controllerSetColor(x+12, y+4, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 		
 		// Mouth Left Lower
 		for (int y=0; y<4; y++)
 			for (int x=0; x<2; x++)
-				ewk.controllerSetColor(x+3, y+11, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+				handler.controllerSetColor(x+3, y+11, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 		
 		// Mouth Left Middle
 		for (int y=0; y<2; y++)
 			for (int x=0; x<2; x++)
-				ewk.controllerSetColor(x+4, y+14, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+				handler.controllerSetColor(x+4, y+14, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 		
 		// Mouth Middle
 		for (int y=0; y<2; y++)
 			for (int x=0; x<7; x++)
-				ewk.controllerSetColor(x+6, y+15, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+				handler.controllerSetColor(x+6, y+15, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 		
 		// Mouth Right Middle
 		for (int y=0; y<2; y++)
 			for (int x=0; x<2; x++)
-				ewk.controllerSetColor(x+13, y+14, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+				handler.controllerSetColor(x+13, y+14, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 		
 		// Mouth Right Lower
 		for (int y=0; y<4; y++)
 			for (int x=0; x<2; x++)
-				ewk.controllerSetColor(x+14, y+11, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
+				handler.controllerSetColor(x+14, y+11, (flashing ? ColorCodes.WHITE : ColorCodes.BLACK).getColor());
 		
 		controller.updateBoard();
 	}
